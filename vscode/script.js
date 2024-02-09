f5d4ce6c517e4e44315b89847b9c062ba8c8cb7e
igormedeiross <igormeds@gmail.com>
2024-02-09 12:52:15 +0000

    :tada: hmtl script

--- vscode/script.js
+++ vscode/script.js
@@ -8,7 +8,7 @@ function toggleMode() {
     // substituir a imagem
     if(html.classList.contains('light')){
         // se tiver light mode, adicionar a imagem light
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('src', './assets/avatar.png')
         //se tiver sem light mode, manter a imagem normal
     } else {img.setAttribute('src', './assets/avatar.png')
     }
